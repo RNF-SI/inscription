366e2240bee9bbd0e3464f32a3701d7b26cf53f7
Zacharie Moulin <zacharie.moulin@gmail.com>
2026-07-13 16:06:49 +0200

    maj home-rnf et ajustements

--- frontend/src/conf/app.config.ts
+++ frontend/src/conf/app.config.ts
@@ -9,7 +9,7 @@ export const AppConfig = {
     "ID_APPLICATION_GEONATURE": 6,
     "API_ENDPOINT": "http://127.0.0.1:5070",
     "appName": "Plateformes RNF",
-    "appTitle": "Plateformes de réserves naturelles de France",
+    "appTitle": "Plateformes de RNF",
     "appSubTitle": "Tous les outils pour vous accompagner dans vos projets",
     "creditHeaderImage": "RNN Baie de St-Brieuc - © P. PIERRE",
     "features": {
